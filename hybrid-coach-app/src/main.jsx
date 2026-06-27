9e978f8f5dc7fe36902f15ecd631b68009a9c405
CarterM200 <matthewwrcarter@gmail.com>
2026-06-27 18:16:43 +0000

    remove duplicate app folder

--- hybrid-coach-app/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import AuthGate from "./AuthGate";
-
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <AuthGate />
-  </React.StrictMode>
-);
